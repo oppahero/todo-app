c03efc304e309c4e0d037b461d804dd312b65763
María López <37983313+oppahero@users.noreply.github.com>
2024-04-24 14:49:31 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InputTaskComponent } from './components/input-task/input-task.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InputTaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todo-app';
+  
 }
---
+++ src/app/components/pending-tasks/pending-tasks.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pending-tasks',
+  standalone: true,
+  imports: [],
+  templateUrl: './pending-tasks.component.html',
+  styleUrl: './pending-tasks.component.css'
+})
+export class PendingTasksComponent {
+
+}
