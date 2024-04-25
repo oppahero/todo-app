0bc73bf17cf559e1ab4ff19af4f1c5254851da87
María López <37983313+oppahero@users.noreply.github.com>
2024-04-25 13:57:47 -0400

    text decoration when task is ready

--- src/app/components/list-task/list-task.component.ts
+++ src/app/components/list-task/list-task.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Task } from '../../models/task';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-list-task',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   template: `
     <ul>
       @for( task of tasks; track task.id){
@@ -16,7 +17,13 @@ import { FormsModule } from '@angular/forms';
           (ngModelChange)="onCheckboxChange()"
           (click)="onCheckboxChange()"
         />
-        <p>{{ task.description }}</p>
+        <p
+          [ngStyle]="{
+            'text-decoration': task.completed ? 'line-through' : 'none'
+          }"
+        >
+          {{ task.description }}
+        </p>
         <input class="remove-bottom" type="button" value="❌" />
       </li>
       }
@@ -25,13 +32,11 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './list-task.component.css',
 })
 export class ListTaskComponent {
+  @Output() noCompletedEvent = new EventEmitter()
 
-  @Output() noCompletedEvent = new EventEmitter();
+  @Input() tasks: Task[] = []
 
-  @Input() tasks: Task[] = [];
-
-  onCheckboxChange(){
+  onCheckboxChange() {
     this.noCompletedEvent.emit()
   }
-
 }
