288fcdd001186ccb4c22f20ee89cd4a787a83e56
María López <37983313+oppahero@users.noreply.github.com>
2024-04-25 14:56:19 -0400

    canvas-confetti: celebrate when all task are completed

--- src/app/components/completed-tasks/completed-tasks.component.ts
+++ src/app/components/completed-tasks/completed-tasks.component.ts
@@ -1,19 +1,22 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ListTaskComponent } from '../list-task/list-task.component';
 import { Task } from '../../models/task';
-
+import confetti from 'canvas-confetti'
 @Component({
   selector: 'app-completed-tasks',
   standalone: true,
   imports: [ListTaskComponent],
   template: `
     @if (completedTasks.length > 0) {
-    <h2>Already done</h2>
-    <app-list-task
-      [tasks]="completedTasks"
-      (noCompletedEvent)="emitChange()"
-      (deleteTaskEvent)="deleteTask($event)"
-    />
+      <div class="info">
+        <h2>Already done</h2>
+        <p>{{ completedTasks.length }}/{{ tasksList }}</p>
+      </div>
+      <app-list-task
+        [tasks]="completedTasks"
+        (noCompletedEvent)="emitChange()"
+        (deleteTaskEvent)="deleteTask($event)"
+      />
     }
   `,
   styleUrl: './completed-tasks.component.css'
@@ -22,12 +25,17 @@ import { Task } from '../../models/task';
 export class CompletedTasksComponent {
   
   completedTasks: Task[] = []
+  tasksList: number = 0
 
   @Output() changeEvent = new EventEmitter()
   @Output() deleteTaskEvent = new EventEmitter<string>()
 
   @Input() set tasks(tasks: Task[]) {    
-    this.completedTasks = tasks.filter((task) => task.completed)    
+    this.tasksList = tasks?.length
+    this.completedTasks = tasks.filter((task) => task.completed) 
+    
+    if(this.tasksList === this.completedTasks.length)
+      this.celebrate()
   }
 
   emitChange() {    
@@ -36,5 +44,17 @@ export class CompletedTasksComponent {
 
   deleteTask(id: string){
     this.deleteTaskEvent.emit(id)
+  }
+
+  celebrate() {
+    const duration = 3000; 
+  
+    confetti({
+      particleCount: 100,
+      spread: 160,
+      origin: { y: 0.6 },
+    });
+  
+    setTimeout(() => confetti.reset(), duration);
   }
 }
